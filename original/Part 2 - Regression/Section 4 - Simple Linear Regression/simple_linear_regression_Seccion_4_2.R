f3936bf7940c98da1bccd8dc383dfa82850a3154
itzchav <itzchav3001@gmail.com>
2025-03-15 10:53:32 -0600

    Seccion 4 - Regresión Lineal 2

--- original/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression_Seccion_4_2.R
+++ original/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression_Seccion_4_2.R
@@ -9,16 +9,16 @@ split = sample.split(dataset$Salary, SplitRatio = 2/3)
 training_set = subset(dataset, split == TRUE)
 testing_set = subset(dataset, split == FALSE)
 
-#Ajustar el modelo de regresiï¿½n linea simple con el conjunto de entrenamiento 
+#Ajustar el modelo de regresión linea simple con el conjunto de entrenamiento 
 regressor = lm(formula = Salary ~ YearsExperience, 
                data = training_set)
 #summary(regressor)
 y_pred = predict(regressor,newdata = testing_set)
 
 #install.packages("ggplot2")
-#Validaciï¿½n de los resultados en el conjunto de entrenamiento
+#Validación de los resultados en el conjunto de entrenamiento
 library(ggplot2)
 ggplot(training_set, aes(x = YearsExperience, y = Salary)) +
   geom_point(colour = "red") +  # Graficar los puntos
-  geom_line(aes(x = YearsExperience, y = predict(regressor,newdata = training_set)), colour = "blue")  # Agregar la lï¿½nea de regresiï¿½n
+  geom_line(aes(x = YearsExperience, y = predict(regressor,newdata = training_set)), colour = "blue")  # Agregar la línea de regresión
 
